ad285a843db8e791c0a4336a2638539ef69494bd
Rachid Assouani <rachidassouani@idmedia24.de>
2025-04-12 19:04:26 +0100

    added logger info for all endpoints

--- src/main/java/io/rachidassouani/product_service/product/ProductController.java
+++ src/main/java/io/rachidassouani/product_service/product/ProductController.java
@@ -1,6 +1,8 @@
 package io.rachidassouani.product_service.product;
 
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,8 @@ import java.util.List;
 @RequestMapping("api/v1/products")
 public class ProductController {
 
+        private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -18,22 +22,26 @@ public class ProductController {
 
     @GetMapping
     public List<ProductDTO> findAllProducts() {
+        logger.info("Received request to find all products");
         return productService.findAllProducts();
     }
 
     @GetMapping("{productId}")
     public ProductDTO findProductById(@PathVariable("productId") Long productId) {
+        logger.info("Received request to find product with ID: {}", productId);
         return productService.findProductById(productId);
     }
 
     @PostMapping
     public ResponseEntity<?> registerProduct(@Valid @RequestBody ProductRegistrationRequest registrationRequest) {
+        logger.info("Received request to save product {} ", registrationRequest);
         productService.saveProduct(registrationRequest);
         return ResponseEntity.ok("Product registered");
     }
 
     @DeleteMapping("{productId}")
     public ResponseEntity<?> deleteProduct(@PathVariable("productId") Long productId) {
+        logger.info("Received request to delete product with ID: {}", productId);
         productService.deleteProductById(productId);
         return ResponseEntity.ok().body("Product Deleted");
     }
@@ -41,6 +49,7 @@ public class ProductController {
     @PutMapping("{productId}")
     public ResponseEntity<?> updateProduct(@PathVariable("productId") Long productId,
                                @Valid @RequestBody ProductUpdateRequest productUpdateRequest) {
+        logger.info("Received request to update product with ID: {}", productId);
         productService.updateProduct(productId, productUpdateRequest);
         return ResponseEntity.ok().body("Product Updated");
     }
